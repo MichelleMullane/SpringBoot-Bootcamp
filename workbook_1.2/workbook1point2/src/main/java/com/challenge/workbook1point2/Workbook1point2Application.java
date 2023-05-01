bc72bd39206f72163313539ffa7d85fa74eaae88
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-05-01 08:47:10 -0400

    completed workbook 1.2

---
+++ workbook_1.2/workbook1point2/src/main/java/com/challenge/workbook1point2/Workbook1point2Application.java
@@ -0,0 +1,13 @@
+package com.challenge.workbook1point2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workbook1point2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workbook1point2Application.class, args);
+	}
+
+}
---
+++ workbook_1.2/workbook1point2/src/test/java/com/challenge/workbook1point2/Workbook1point2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.workbook1point2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workbook1point2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
